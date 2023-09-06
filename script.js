ab6f2b323357177f5931f4610c91942692966a76
Magamed <132371413+AndyMagwayer@users.noreply.github.com>
2023-09-06 14:53:41 +0000

    fix: fixed id bags

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.addEventListener("load", function() {
   document.getElementById("myElement").innerHTML = "Page Loaded";
 });
 
-document.getElementById("myElement").addEventListener("touchstart", function() {
+document.getElementById("myElement1").addEventListener("touchstart", function() {
   this.style.opacity = "0.5";
 });
  
-document.getElementById("myElement").addEventListener("drag", function() {
+document.getElementById("myElement2").addEventListener("drag", function() {
   this.style.transform = "rotate(45deg)";
 });
