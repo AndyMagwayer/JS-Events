b3268b53bf87debc0697c6f2c941244205594bed
Magamed <132371413+AndyMagwayer@users.noreply.github.com>
2023-09-06 14:52:28 +0000

    feat: sixth as sa drag

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ window.addEventListener("load", function() {
 document.getElementById("myElement").addEventListener("touchstart", function() {
   this.style.opacity = "0.5";
 });
+ 
+document.getElementById("myElement").addEventListener("drag", function() {
+  this.style.transform = "rotate(45deg)";
+});
