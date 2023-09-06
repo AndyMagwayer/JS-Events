a9327b614b8c154a4a76bb706a4d62523984b5b1
Magamed <132371413+AndyMagwayer@users.noreply.github.com>
2023-09-06 14:51:16 +0000

    feat: added fifth event as a touchstart

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function() {
   document.getElementById("myElement").innerHTML = "Page Loaded";
 });
+
+document.getElementById("myElement").addEventListener("touchstart", function() {
+  this.style.opacity = "0.5";
+});
